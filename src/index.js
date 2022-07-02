23802fb5970737f940476306c76b5480aba9d59f
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-07-02 15:05:35 -0300

    feat: add account creation route

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
 const express = require("express");
+const { v4: uuidV4 } = require("uuid");
 
 const app = express();
+app.use(express.json());
+
+const customers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement - array[]
+ */
+app.post("/account", (req, res) => {
+  const { cpf, name, statement } = req.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return res.status(400).json({
+      error: "Customer already exists",
+    });
+  }
+
+  const customerObject = {
+    id: uuidV4(),
+    cpf,
+    name,
+    statement: [],
+  };
+
+  customers.push(customerObject);
+
+  return res.status(201).json(customerObject);
+});
 
 app.listen(3333);
